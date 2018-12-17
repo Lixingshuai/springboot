b29cb42abdea117d760189f3ae3e89006dba10ec
lixingshuai <735543130@qq.com>
2018-12-17 14:30:24 +0800

    支持文件上传 /upload

---
+++ src/main/java/com/demo/springBoot/controller/UploadController.java
@@ -0,0 +1,49 @@
+package com.demo.springBoot.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class UploadController {
+
+	private final String UPLOADED_FOLDER = "";
+
+	@RequestMapping("/toUpload")
+	public String toUpload() {
+		return "/upload/upload";
+	}
+
+	@RequestMapping("/uploadStatus")
+	public String uploadStatus() {
+		return "/upload/uploadResult";
+	}
+
+	@PostMapping("/upload")
+	public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+		if (file.isEmpty()) {
+			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+			return "redirect:uploadStatus";
+		}
+		try {
+			// Get the file and save it somewhere
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+			Files.write(path, bytes);
+			redirectAttributes.addFlashAttribute("message",
+					"You successfully uploaded '" + file.getOriginalFilename() + "'");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "redirect:/uploadStatus";
+	}
+
+}
